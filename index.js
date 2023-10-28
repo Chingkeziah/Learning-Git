cae116c6e80834da0c8f9aa4c38760ccd5c54d0d
Ching Keziah <chingkeziah@gmail.com>
2023-10-28 07:00:34 -0400

    my first JavaScrip file

---
+++ index.js
@@ -0,0 +1,28 @@
+// A prompt form
+var a=prompt("what is your name");
+var b=prompt("Enter the first number");
+var c=prompt("Enter the second number");
+// multiplying the elements in the second and third variables
+var x=b*c;
+// a document object enables or contains text displayed on the browser, just like in html text can only display when kept in the body
+document.write("the product is", x);
+// to insert a paragraph in Javascript you use the Paragraph tag from html inside the string(sentence)
+document.write("<p>Welcome to Javascript lessons by Albright Academy");
+document.write("<br>");
+document.write("<b>HI there");
+document.write("<br>");
+str1="Ching";
+str2="Keziah";
+document.write("<p>Welcome, ", str1+str2);
+// the windows object refers you to another site when you open your page
+win=window.open("mywebsite.html");
+// conditionals i.e if, else, if, switch, :?, loop, while-loop, do while, for, repeat until
+let counter = 0;
+function count(){
+    counter++;
+alert(counter);
+                    }
+console.log("this is something"); 
+console.error("this is an error");
+console.warning("this is a warning");
+            
